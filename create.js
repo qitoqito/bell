5e7d535cd7c5b74875f7cc43c5bba06cbc4f79c0
可达鸭 <ii@iippcc.com>
2025-02-10 22:53:59 +0800

    测试青龙写入

---
+++ create.js
@@ -0,0 +1,33 @@
+import fs from 'fs'
+import path from 'path';
+import {
+    fileURLToPath
+} from 'url';
+import ini from 'ini'
+import CryptoJS from 'crypto-js'
+
+function md5(encryptString) {
+    return CryptoJS.MD5(encryptString.toString()).toString()
+}
+for (let i =0;i<6;i++) {
+    let script=md5(i.toString())
+    let code = `
+// new Env('test${script}')
+// 6 6 6 6 6  jd_${script}.js
+
+import path from 'path';
+import {
+    fileURLToPath
+} from 'url';
+!(async () => {
+    console.log("test")
+})().catch((e) => {
+    console.log(e.message)
+})`
+    fs.writeFile(`${script}.js`, code, function(err, data) {
+        if (err) {
+            throw err;
+        }
+        console.log(`🐯‍❄️ 写入 ${script}`)
+    })
+}
